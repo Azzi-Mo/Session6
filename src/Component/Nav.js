72167b8dbeb8350299ce8429a11cbb071bd0ab4f
Azzi-MO <alhwarymostafa@gmail.com>
2022-11-12 21:25:29 -0800

    tableedite done

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -24,7 +24,7 @@ function Nav() {
           <ul className="navbar-nav">
             <li className="nav-item active">
               <a className="nav-link" href="/#">
-                Home <span className="sr-only">(current)</span>
+                Home 
               </a>
             </li>
           </ul>
--- src/Component/Pages/products.js
+++ src/Component/Pages/products.js
@@ -1,11 +1,26 @@
-function Products()
-{
-    return(
+function Products() {
+  return (
+    <>
+      <h5>products page</h5>
 
-        <>
-              <h1>products page</h1>
-        </>
-
-    )
+      <table className="table table-striped">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Product 1</td>
+            <td>100</td>
+          </tr>
+      
+        </tbody>
+      </table>
+    </>
+  );
 }
-export default Products
+export default Products;
